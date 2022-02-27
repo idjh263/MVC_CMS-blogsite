8f453e6f3752cb71603f143efb0784d9f4d9e67c
idjh263 <idjh263@gmail.com>
2022-02-27 10:34:22 -0500

    update connection

--- utils/auth.js
+++ utils/auth.js
@@ -1,4 +1,4 @@
-// middleware to verify user loggied in before restricted route access given
+
 const withAuth = (req, res, next) => {
     if (!req.session.user_id) {
       res.redirect("/login");
