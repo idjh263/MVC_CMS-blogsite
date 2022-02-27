79f7dea6331aadfe810d92b774703264e82fe216
idjh263 <idjh263@gmail.com>
2022-02-27 11:40:00 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-//allow for use of .env data
+
 require('dotenv').config();
 
-// create connection to db, whether local on on heroku using jawsdb
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
